7dc7a99b12067222047e1eb017083eff1974be96
Soutrik Chowdhury <soutrik.chowdhury@ab-inbev.com>
2022-10-27 18:47:40 +0000

    First Commit

---
+++ demo.py
@@ -0,0 +1,18 @@
+# Example from Visual Studio Code's official website.
+
+import numpy as np
+import matplotlib.pyplot as plt
+from utils.rand_func import my_sum, my_sub
+
+print("hello")
+x = "Rahul"
+print(f"Name is {x}")
+x = np.linspace(
+    0, 20, 100
+)  # Create a list of evenly-spaced numbers over the range
+plt.plot(x, np.sin(x))  # Plot the sine of each x point
+plt.show()  # Display the plot
+
+print(my_sum(1, 2, 3))
+print(my_sub(7, 2, 4))
+
