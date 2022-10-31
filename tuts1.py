a310042b37fb8b72ce2bf88399fe01527fbc06da
Soutrik <soutrik.chowdhury@ab-inbev.com>
2022-10-31 05:08:37 +0000

    First Commit

---
+++ tuts1.py
@@ -0,0 +1,11 @@
+a = "I am the one whom you are looking for"
+b = "This is not the time to reveal yourself"
+print(a + "-" + b)
+x = 123
+y = 3445
+k = 7776
+print(x + y + k)
+# TODO
+# something left to be done
+# FIXME
+# fix something in the code
