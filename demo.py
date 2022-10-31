d92d0fc6f6532d30b19f4eea63495dc4b8da569f
Soutrik <soutrik.chowdhury@ab-inbev.com>
2022-10-31 15:17:57 +0000

    Tracking conflicts

--- demo.py
+++ demo.py
@@ -8,7 +8,7 @@ print("hello")
 x = "Rahul"
 print(f"Name is {x}")
 x = np.linspace(
-    0, 20, 100
+    0, 10, 100
 )  # Create a list of evenly-spaced numbers over the range
 plt.plot(x, np.sin(x))  # Plot the sine of each x point
 plt.show()  # Display the plot
